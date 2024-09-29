8644849f088b0c346fbf35189bf62f4e00ac9ccf
Saurav Suman <sauravsuman689@gmail.com>
2024-09-29 12:54:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! - This is Saurav Lab for Node js application'));
+app.get('/', (req, res) => res.send('Hello World! - This is Saurav Lab for Node js application deployment'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
