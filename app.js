cc0adea3adcf2f9fff86cd5d397678630be6852e
Saurav Suman <sauravsuman689@gmail.com>
2024-09-29 12:51:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! - Upgrad The LAB ENV IS USing'));
+app.get('/', (req, res) => res.send('Hello World! - This is Saurav Lab for Node js application'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
